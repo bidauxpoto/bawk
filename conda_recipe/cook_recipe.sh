b9b314c5a0ee6db21bcd619b7673dc8d41932a45
Marco-Masera <mirco.masera@gmail.com>
2023-01-13 11:22:41 +0100

    .

--- conda_recipe/cook_recipe.sh
+++ conda_recipe/cook_recipe.sh
@@ -3,6 +3,7 @@
 conda-build .
 #update path and version!
 conda convert --platform all /home/mmasera_local/anaconda3/conda-bld/linux-64/bawk-1.1-pl5262_0.tar.bz2 -o outdir/
+anaconda upload --user molinerislab /home/mmasera_local/anaconda3/conda-bld/linux-64/bawk-1.1-pl5262_0.tar.bz2
 anaconda upload --user molinerislab outdir/linux-32/bawk-1.1-pl5262_0.tar.bz2
 anaconda upload --user molinerislab outdir/linux-32/bawk-1.1-pl5262_0.tar.bz2
 anaconda upload --user molinerislab outdir/linux-aarch64/bawk-1.1-pl5262_0.tar.bz2
